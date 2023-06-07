b515082c2d6379bd728eb5e8f7091040013869cc
JaimeGSandoval <jaime.devs@gmail.com>
2023-06-07 15:33:51 -0700

    add js dir

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const driverName = document.querySelector('.driverName');
+const driverConstructor = document.querySelector('.driverConstructor');
+const driverPoints = document.querySelector('.driverPoints');
+const driverBtn = document.querySelector('button');
+
+const getDriver = async () => {
+  const driverInput = document.querySelector('input').value;
+
+  try {
+    const response = await fetch(`http://localhost:8000/driver/${driverInput}`);
+    const driverData = await response.json();
+
+    driverName.innerText = `Name: ${driverData.name}`;
+    driverConstructor.innerText = `Constructor: ${driverData.constructor}`;
+    driverPoints.innerText = `Points: ${driverData.points}`;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+driverBtn.addEventListener('click', getDriver);
